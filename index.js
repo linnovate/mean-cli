de6a5bd6ef02b590ddf80fef1d0961fc920a7675
Drew Fyock <fyockm@gmail.com>
2014-05-04 20:25:40 -0400

    switch to 4 space indent for consistency with main mean repo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var meanVersion = require(process.cwd() + '/package.json').version;
 
 if (meanVersion < '0.4.0') {
-  console.error('Please upgrade mean to 0.4.0!');
+    console.error('Please upgrade mean to 0.4.0!');
 } else {
-	//all older versions of mean
-	module.exports = require('./lib/mean');
+    //all older versions of mean
+    module.exports = require('./lib/mean');
 }
