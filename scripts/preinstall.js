5ee8bf5a191889eb6ae591f9d4161a029d6e4fe4
Drew Fyock <fyockm@gmail.com>
2014-10-20 09:15:51 -0400

    Combine meanio and mean-cli preinstall scripts

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,23 +1,34 @@
-var spawn = require('child_process').spawn;
-    list = spawn('npm', ['ls', '--global', '--parseable', '--depth', '0']),
-    grep  = spawn('grep', ['meanio']);
+var args = process.argv.slice(2);
 
-list.stdout.on('data', function (data) {
-  grep.stdin.write(data);
-});
+if (~args.indexOf('meanio') && (~args.indexOf('-g') || ~args.indexOf('--global'))) {
+  console.log('Please install \'mean-cli\' globally instead of \'meanio\' as:');
+  console.log('');
+  console.log('  $ npm install -g mean-cli');
+  console.log('');
+  process.exit(1);
+} else if (~args.indexOf('mean-cli')) {
+  var spawn = require('child_process').spawn;
+  npmls = spawn('npm', ['ls', '--global', '--parseable', '--depth', '0']),
+  grep = spawn('grep', ['meanio']);
 
-list.on('close', function (code) {
-  grep.stdin.end();
-});
+  npmls.stdout.on('data', function(data) {
+    grep.stdin.write(data);
+  });
 
-grep.stdout.on('data', function (data) {
-  console.log('' + data);
-});
+  npmls.on('close', function(code) {
+    grep.stdin.end();
+  });
 
-grep.on('close', function (code) {
-  if (code === 0) {
-    console.log('  Please run \'npm uninstall -g meanio\' prior to installing mean-cli');
-    console.log('');
-    process.exit(1);
-  }
-});
+  grep.stdout.on('data', function(data) {
+    console.log('' + data);
+  });
+
+  grep.on('close', function(code) {
+    // meanio global was found
+    if (code === 0) {
+      console.log('  Please run \'npm uninstall -g meanio\' prior to installing mean-cli');
+      console.log('');
+      process.exit(1);
+    }
+  });
+}
