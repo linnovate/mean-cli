9bc0031d6bc8fc06030bc60b57dda84b36fae7d1
brownman <brownman556@gmail.com>
2014-12-18 15:09:49 +0200

    travis: test both: grunt/gulp methods
    [skip ci]

--- start.sh
+++ start.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
+set -u
 
 test_self(){
-sudo apt-get install -y -q curl
-npm install bower
-npm install -g
-npm link
+  sudo apt-get install -y -q curl
+  npm install bower
+  npm install -g
+  npm link
+  npm test
+}
 
-npm test
+do_as_instructed(){
+  cd myApp
+  npm install -g
+  npm link
 }
 
 test_mean_init(){
-echo -e '\n' |  mean init myApp
-cd myApp
-#npm -g install
-npm install -g
-npm link
-grunt test && (
-grunt &
-while true; do  curl 0.0.0.0:3000 2>/dev/null && break ; sleep 1 ; done
-)
+  local tasker=${TASKER:-gulp}
+  if [ "$tasker" = 'grunt' ];then
+    echo -e '\n' |  mean init myApp
+    do_as_instructed
+    grunt test && ( grunt &)
+  else
+    echo -e '\n' |  mean init myApp
+    do_as_instructed
+    gulp test && ( gulp &)
+  fi
 }
 
-test_self
-test_mean_init
+test_navigation(){
+  while true; do  curl 0.0.0.0:3000 2>/dev/null && break ; sleep 1 ; done
+}
+
+cmd_start="$1"
+eval "$cmd_start"
