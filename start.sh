759955a0ac8b065d2d399383080b74a17eef647c
brownman <brownman556@gmail.com>
2014-12-23 11:51:16 +0200

    find tool: xcowsay

--- start.sh
+++ start.sh
@@ -1,46 +1,52 @@
 #!/bin/bash
 set -u
 
+set_env(){
+export PATH="$PATH:/usr/games/"
+}
+
+
 test_self(){
-  sudo apt-get install -y -q curl
-  npm install bower
-  npm install -g
-  npm link
-  npm test
+sudo apt-get install -y -q curl
+npm install bower
+npm install -g
+npm link
+npm test
 }
 
 user_instructions(){
-  npm install -g
-  npm link .
-  mean status
-  grunt test
-  grunt &
+npm install -g
+npm link .
+mean status
+grunt test
+grunt &
 }
 
 test_navigation(){
-  while true; do  curl 0.0.0.0:3000 2>/dev/null && break ; sleep 1 ; done
+while true; do  curl 0.0.0.0:3000 2>/dev/null && break ; sleep 1 ; done
 }
 
 test_mean_init(){
-    echo -e '\n' |  mean init myApp
-    cd myApp
-    user_instructions
-    test_navigation
+echo -e '\n' |  mean init myApp
+cd myApp
+user_instructions
+test_navigation
 }
 
 tests(){
-  test_self
-  test_mean_init
+test_self
+test_mean_init
 }
 capture1(){
-  chmod +x ./capture.sh
-  bash -c ./capture.sh
-  cp /tmp/session* $CIRCLE_ARTIFACTS/
-  }
-  
+chmod +x ./capture.sh
+bash -c ./capture.sh
+cp /tmp/session* $CIRCLE_ARTIFACTS/
+}
+
 steps(){
+set_env
 test "$LOGNAME" != travis  && capture1
-  #tests
+#tests
 }
 
 steps
