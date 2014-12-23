78b46e4d98391b21d2dbd79a03c67fc79a54b11a
brownman <brownman556@gmail.com>
2014-12-23 14:39:02 +0200

    debug: print png to console using ascii-art

--- capture.sh
+++ capture.sh
@@ -12,18 +12,39 @@ ensure1(){
 test -d $dir_product || { mkdir -p $dir_product; }
 }
 apt0(){
-sudo apt-get install -y -q <<START
+sudo apt-get install -y -q <<START1
 xcowsay  libnotify-bin imagemagick
 xvfb x11-utils x11-apps dbus-x11  
-START
+START1
+}
+print_single(){
+local file=$1
+node <<START2
+require("image-to-ascii")("$file", function (err, result) {
+    console.log(result);
+})
+START2
+}
+
+print_many(){
+  local list=$( ls -1 $dir_product/*sh )
+    for item in $list;do
+    file=$dir_product/$item
+    test -f $file
+    print_single $file
+    done
+
+
 }
 
 apt1(){  
 #firefox
-commander sudo apt-get -y update
+commander sudo apt-get -y -q update
 npm install -g image-to-ascii
-commander sudo apt-get -y -q install firefox graphicsmagick imagemagick xvfb x11-utils x11-apps   dbus-x11  xcowsay   libnotify-bin graphicsmagick
-sudo apt-get update --fix-broken
+
+commander sudo apt-get install -y -q xvfb x11-utils x11-apps   dbus-x11 
+commander sudo apt-get install -y -q graphicsmagick  
+commander sudo apt-get install -y -q xcowsay   libnotify-bin firefox imagemagick
 }
 
 
@@ -62,6 +83,7 @@ steps(){
   debug_screen
   capture2 &
   sleep 5
+  print_many
   cp $dir_product/*.png $dir_artifacts
 }
 
