1b7b1005665b15e1f7c76d0bc14c7be0a5f72a1d
David Cook <david.cook@autodesk.com>
2016-10-20 22:20:52 -0400

    format code to .editorconfig standards

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function() {
-	var config  = {
-		napiUrl: 'https://network.mean.io/api/v0.1'
-	};
-	return config;
+  var config = {
+    napiUrl: 'https://network.mean.io/api/v0.1'
+  };
+  return config;
 };
